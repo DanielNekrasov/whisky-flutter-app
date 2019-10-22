0a707050c8343951c3e15c2d85858875c8b4322e
Daniel Nekrasov <nekrasov.danny@gmail.com>
2019-10-22 23:03:21 +0500

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:whisky_app/data/app_state.dart';
 import 'package:whisky_app/screens/home.dart';
 import 'package:whisky_app/styles.dart';
@@ -14,8 +16,23 @@ void main() {
   runApp(WhiskyApp());
 }
 
-class WhiskyApp extends StatelessWidget {
+class WhiskyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _WhiskyAppState createState() => _WhiskyAppState();
+}
+
+class _WhiskyAppState extends State<WhiskyApp> {
+  @override
+  void initState() {
+    super.initState();
+    final databaseReference = FirebaseDatabase.instance.reference();
+    final whiskies = databaseReference.child('whiskies');
+    whiskies.once().then((DataSnapshot snapshot) {
+      print('Data : ${snapshot.value}');
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AppState>(
